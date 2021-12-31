4537f24d07544c50696ab1f11251ac67026d384a
Programmer-RD-AI <aigithub@gmail.com>
2021-12-31 16:41:28 +0530

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/HYLS/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
