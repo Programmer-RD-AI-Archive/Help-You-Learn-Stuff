f1dc04a770a5b93eeff5838b4c805f10259e151e
Programmer-RD-AI <aigithub@gmail.com>
2021-12-31 22:58:58 +0530

    Update home.py

--- API/routes/home.py
+++ API/routes/home.py
@@ -1,6 +1,6 @@
 from API import *
 from API.help_funcs import send_email
-from example.azure_sql import Azure_SQL
+from API.db.azure_sql import Azure_SQL
 
 contact_us_request_parser = reqparse.RequestParser()
 contact_us_request_parser.add_argument(
@@ -11,15 +11,15 @@ contact_us_request_parser.add_argument(
 )
 
 
-class Get_Config(Resource):
+class Contact_Us(Resource):
     def get(self):
         args = contact_us_request_parser.parse_args()
         email = args["email"]
         question = args["question"]
         send_email(subject=question, message=f"Email - {email} \n Question {question}")
         asql = Azure_SQL()
-        asql.create_new_table("CREATE TABLE Contact_Us_Questions (Email varchar(50), Question varchar(50))")
-        
+        asql.insert_one(f"INSERT INTO [TEST]( [Email], [Question] ) VALUES ( {email}, {question})")
+        return {"message": "Success"}
 
 
-api.add_resource(Get_Config, "/api/get_config")
+api.add_resource(Contact_Us, "/api/Contact_Us")
