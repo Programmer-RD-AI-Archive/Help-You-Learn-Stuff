b9c8b5d0d617dc5f0a99651254087d197ac2b38e
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 14:14:20 +0530

    Update home.py

--- WEB/routes/home.py
+++ WEB/routes/home.py
@@ -6,10 +6,26 @@ def home():
     if request.method == "POST":
         email = request.form["email"]
         question = request.form["Question"]
-        config = requests.get(
+        config = requests.post(
             "http://127.0.0.1:5000/api/Contact_Us", {"email": email, "question": question}
         )
-        
+        config = config.json()
+        print("*" * 50)
+        print(config)
+        print("*" * 50)
+        if config["message"] is True:
+            try:
+                session.pop("email")
+                session.pop("question")
+            except:
+                pass
+            flash("Your Question will be answered as soon as possible by our team.", "success")
+            return redirect("/")
+        else:
+            session["email"] = email
+            session["question"] = question
+            flash("There is some error so please try again.", "danger")
+            return redirect("/")
     password = "01x2253x6871"
     config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
     config = config.json()
@@ -22,4 +38,16 @@ def home():
         description_1=config["config"]["Home"]["Help you Learn Stuff"]["Description"],
         description_2=config["config"]["Home"]["About Us"]["Description"],
         description_3=config["config"]["Home"]["Contact Us"]["Description"],
+        session=session,
     )
+
+
+@app.route("/Sign/Up", methods=["GET", "POST"])
+def sign_up():
+    password = "01x2253x6871"
+    config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
+    config = config.json()
+    print(config)
+    if request.method == "POST":
+        pass
+    return render_template("home/sign_up.html", session=session,config=config)
