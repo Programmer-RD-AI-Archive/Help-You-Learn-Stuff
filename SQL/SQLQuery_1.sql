3a87417138c5a6c07323582746026ff2dab7d041
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 14:52:13 +0530

    Update SQLQuery_1.sql

--- SQL/SQLQuery_1.sql
+++ SQL/SQLQuery_1.sql
@@ -1,1 +1,15 @@
-SELECT table_name FROM information_schema.tables
+DROP TABLE TEST
+CREATE TABLE TEST  
+(  
+ id_num int IDENTITY(1,1),  
+ fname varchar (20),  
+ minit char(1),  
+ lname varchar(30)  
+);  
+  
+INSERT TEST  
+   (fname, minit, lname)  
+VALUES  
+   ('Karin', 'F', 'Josephs');  
+  
+SELECT * FROM TEST
