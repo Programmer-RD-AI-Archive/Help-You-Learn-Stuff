64f5f8158ebe74325c584a5dd622785d2044bc08
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 16:21:17 +0530

    Update SQLQuery_1.sql

--- SQL/SQLQuery_1.sql
+++ SQL/SQLQuery_1.sql
@@ -1,15 +1,20 @@
-DROP TABLE TEST
-CREATE TABLE TEST  
-(  
- id_num int IDENTITY(1,1),  
- fname varchar (20),  
- minit char(1),  
- lname varchar(30)  
-);  
+-- DROP TABLE TEST
+-- CREATE TABLE TEST  
+-- (  
+--  id_num int IDENTITY(1,1),  
+--  fname varchar (20),  
+--  minit char(1),  
+--  lname varchar(30)  
+-- );  
   
-INSERT TEST  
-   (fname, minit, lname)  
-VALUES  
-   ('Karin', 'F', 'Josephs');  
+-- INSERT TEST  
+--    (fname, minit, lname)  
+-- VALUES  
+--    ('Karin', 'F', 'Josephs');  
   
-SELECT * FROM TEST
+-- SELECT * FROM TEST
+-- DROP TABLE Contact_Us
+-- DROP TABLE TEST
+-- SELECT table_name FROM information_schema.tables
+-- CREATE TABLE Accounts (ID int IDENTITY(1,1), Email varchar(max),User_Name varchar(max), Password varchar(max))
+SELECT * FROM Accounts
