5fd21e09949d9c99d65b4cb16930a5c59c7d2939
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 23:49:59 +0530

    Update dashboard.py

--- WEB/routes/dashboard.py
+++ WEB/routes/dashboard.py
@@ -3,5 +3,10 @@ from WEB.help_funcs import *
 
 
 @app.route("/Usr/<_id>", methods=["GET", "POST"])
+@app.route("/Usr/<_id>/", methods=["GET", "POST"])
 def usr_home(_id):
-    pass
+    if str(_id) == str(session["id"]):
+        password = "01x2253x6871"
+        config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
+        config = config.json()
+        return render_template("dashboard/home.html", session=session, config=config)
