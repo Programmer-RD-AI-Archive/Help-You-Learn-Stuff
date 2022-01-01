a95d673f7d31a5cd3e357c4c5cd2d40d76cda3ae
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 14:52:10 +0530

    Update home.py

--- API/routes/home.py
+++ API/routes/home.py
@@ -3,11 +3,9 @@ from API.help_funcs import send_email
 from API.db.azure_sql import Azure_SQL
 
 contact_us_request_parser = reqparse.RequestParser()
+contact_us_request_parser.add_argument("email", type=str, help="email is required", required=True)
 contact_us_request_parser.add_argument(
-    "email", type=str, help="Password is required", required=True
-)
-contact_us_request_parser.add_argument(
-    "question", type=str, help="Password is required", required=True
+    "question", type=str, help="question is required", required=True
 )
 
 
@@ -21,6 +19,11 @@ class Contact_Us(Resource):
             send_email(subject=question, message=f"Email - {email} \n Question {question}")
             print("Email Send")
             asql = Azure_SQL()
+            tables = asql.get_tables()
+            if "Contact_Us" not in tables:
+                asql.create_new_table(
+                    "CREATE TABLE Contact_Us (Email varchar(max),Question varchar(max))"
+                )
             asql.insert_to_table(
                 f"INSERT INTO [Contact_Us]( [Email], [Question] ) VALUES ( '{email}', '{question}')"
             )
@@ -31,11 +34,35 @@ class Contact_Us(Resource):
 
 
 api.add_resource(Contact_Us, "/api/Contact_Us")
+accounts_request_parser = reqparse.RequestParser()
+accounts_request_parser.add_argument("email", type=str, help="email is required", required=True)
+accounts_request_parser.add_argument(
+    "password", type=str, help="Password is required", required=True
+)
+accounts_request_parser.add_argument(
+    "user name", type=str, help="user name is required", required=True
+)
 
 
-class Sign_Up(Resource):
+class Accounts(Resource):
     def get(self):
-        pass
+        asql = Azure_SQL()
+        tables = asql.get_tables()
+        if "Accounts" not in tables:
+            asql.create_new_table(
+                "CREATE TABLE Accounts (ID Email varchar(max),User_Name varchar(max), Password varchar(max))"
+            )
+        accounts = asql.select_table("SELECT * FROM Accounts")
+        return {"message": accounts}
 
     def post(self):
-        pass
+        args = accounts_request_parser.parse_args()
+        asql = Azure_SQL()
+        tables = asql.get_tables()
+        if "Accounts" not in tables:
+            asql.create_new_table(
+                "CREATE TABLE Accounts (Email varchar(max),User_Name varchar(max), Password varchar(max))"
+            )
+        asql.insert_to_table(
+            f"INSERT INTO [Accounts]( [Email], [User_Name], [Password] ) VALUES ( '{args['email']}', '{args['user name']}', '{args['password']}')"
+        )
