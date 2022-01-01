a77ee8e959773e3b32a358c48c2f022513314240
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 14:13:46 +0530

    Update help_funcs.py

--- API/help_funcs.py
+++ API/help_funcs.py
@@ -1,6 +1,22 @@
-from mailer import Mailer
+import smtplib, ssl
 
 
 def send_email(subject="TEST", message="From Python"):
-    mail = Mailer(email="helpyoulearnstuff@gmail.com", password="Ranuga D 2008")
-    mail.send(receiver="go2ranuga@gmail.com", subject=subject, message=message)
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "helpyoulearnstuff@gmail.com"  # Enter your address
+    receiver_email = "go2ranuga@gmail.com"  # Enter receiver address
+    password = "Ranuga D 2008"
+    message = """\
+    Subject: Hi there
+    
+    This message is sent from Python."""
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+
+
+
+
