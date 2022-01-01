5eed333172b22fe79e9cd90b35795d5ccf674b04
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 17:00:45 +0530

    Update __init__.py

--- API/__init__.py
+++ API/__init__.py
@@ -1,26 +1,16 @@
 import json
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+import pyodbc
+import textwrap
+import binascii
+import os, uuid
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+import smtplib, ssl
 
 password = "01x2253x6871"
 app = Flask(__name__)
 api = Api(app)
-get_config_request_parser = reqparse.RequestParser()
-get_config_request_parser.add_argument(
-    "password", type=str, help="Password is required", required=True
-)
-
-
-class Get_Config(Resource):
-    def get(self):
-        args = get_config_request_parser.parse_args()
-        if args["password"] == password:
-            config = open("./API/config.json")
-            config = json.load(config)
-            return {"config": config}
-        else:
-            abort(401, message="Wrong password")
-
-
-api.add_resource(Get_Config, "/api/get_config")
 from API.routes import *
+from API.help_funcs import *
+from API.db import *
