0152107aa5b31ee8ef82c09632983bcbbc27b14c
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 17:01:54 +0530

    Update help_funcs.py

--- API/help_funcs.py
+++ API/help_funcs.py
@@ -1,4 +1,5 @@
-import smtplib, ssl
+from API import *
+import base64
 
 
 def send_email(subject="TEST", message="From Python"):
@@ -7,16 +8,31 @@ def send_email(subject="TEST", message="From Python"):
     sender_email = "helpyoulearnstuff@gmail.com"  # Enter your address
     receiver_email = "go2ranuga@gmail.com"  # Enter receiver address
     password = "Ranuga D 2008"
-    message = """\
-    Subject: Hi there
+    message = f"""\
+    Subject: {subject}
     
-    This message is sent from Python."""
+    {message}"""
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
 
 
+def encode(message: str) -> bytes:
+    """
+    Encode string for privacy and encryption.
+    """
+    msg_bytes = message.encode("latin-1")
+    string_bytes = base64.b64encode(msg_bytes)
+    string = string_bytes.decode("latin-1")
+    return string
 
 
-
+def decode(message: str) -> bytes:
+    """
+    Decode string for privacy and encryption.
+    """
+    msg_bytes = message.encode("latin-1")
+    string_bytes = base64.b64decode(msg_bytes)
+    string = string_bytes.decode("latin-1")
+    return string
