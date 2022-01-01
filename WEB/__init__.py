845aa50ba47bfee28632481334ac3d44b02a9d06
Programmer-RD-AI <aigithub@gmail.com>
2022-01-01 23:49:53 +0530

    Update __init__.py

--- WEB/__init__.py
+++ WEB/__init__.py
@@ -7,5 +7,6 @@ app = Flask(__name__)  # init flask app
 app.debug = True  # debug
 app.secret_key = "Help you Learn Stuff"  # secret key
 app.config["SECURITY_PASSWORD_SALT"] = "Help you Learn Stuff"
+password = "01x2253x6871"
 from WEB.routes import *
 from WEB.help_funcs import *
