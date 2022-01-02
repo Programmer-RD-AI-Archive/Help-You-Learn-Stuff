3ecce1ae288f455fbf410027c4a1c5051b3ac27f
Programmer-RD-AI <aigithub@gmail.com>
2022-01-02 19:29:11 +0530

    Create admin.py

---
+++ WEB/routes/admin.py
@@ -0,0 +1,26 @@
+from requests.sessions import session
+from WEB import *
+from WEB.help_funcs import *
+
+
+@app.route("/Admin", methods=["GET", "POST"])
+@app.route("/Admin/", methods=["GET", "POST"])
+def admin_home():
+    if "Is_Admin" in session:
+        config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
+        config = config.json()
+        return render_template("admin/home.html", config=config, session=session)
+
+
+@app.route("/Admin/Log/Out", methods=["GET", "POST"])
+@app.route("/Admin/Log/Out/", methods=["GET", "POST"])
+def admin_home():
+    if "Is_Admin" in session:
+        session.pop("Is_Admin")
+        session.pop("id")
+        session.pop("User Name")
+        session.pop("Email")
+        session.pop("Rank")
+        session.pop("Password")
+        flash("Loged out as admin", "success")
+        return redirect("/")
