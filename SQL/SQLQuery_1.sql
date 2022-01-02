c3e9a0d8691e801d5b69f749c61c5b6b20961174
Programmer-RD-AI <aigithub@gmail.com>
2022-01-02 16:40:38 +0530

    Update SQLQuery_1.sql

--- SQL/SQLQuery_1.sql
+++ SQL/SQLQuery_1.sql
@@ -1,26 +1,20 @@
 -- DROP TABLE TEST
--- CREATE TABLE TEST  
--- (  
---  id_num int IDENTITY(1,1),  
---  fname varchar (20),  
---  minit char(1),  
---  lname varchar(30)  
--- );  
-  
--- INSERT TEST  
---    (fname, minit, lname)  
--- VALUES  
---    ('Karin', 'F', 'Josephs');  
-  
--- SELECT * FROM TEST
--- DROP TABLE Contact_Us
--- DROP TABLE TEST
--- SELECT table_name FROM information_schema.tables
--- CREATE TABLE Accounts (ID int IDENTITY(1,1), Email varchar(max),User_Name varchar(max), Password varchar(max))
--- SELECT * FROM Accounts
--- CREATE TABLE Contact_Us (ID int IDENTITY(1,1),  Email varchar(max),Question varchar(max))
--- CREATE TABLE Accounts (ID int IDENTITY(1,1), Email varchar(max),User_Name varchar(max), Password varchar(max))
+-- CREATE TABLE TEST
+-- (
+--     Rank int,
+-- );
+-- INSERT INTO [TEST]
+--     ( [Rank] )
+-- VALUES
+--     (1)
+-- SELECT *
+-- FROM TEST
 -- DROP TABLE Accounts
 -- DROP TABLE Contact_Us
 -- SELECT * FROM Accounts
 -- SELECT * FROM Contact_Us
+CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT, Email varchar(max),User_Name varchar(max), Password varchar(max))
+CREATE TABLE Contact_Us (ID int IDENTITY(1,1),  Email varchar(max),Question varchar(max))
+-- DROP TABLE Accounts
+-- DROP TABLE Contact_Us
+-- DROP TABLE TEST
