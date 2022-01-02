bfa4679837cfb6b0d8be25bdbede6768e129b7ff
Programmer-RD-AI <aigithub@gmail.com>
2022-01-02 19:28:59 +0530

    Update SQLQuery_1.sql

--- SQL/SQLQuery_1.sql
+++ SQL/SQLQuery_1.sql
@@ -11,10 +11,15 @@
 -- FROM TEST
 -- DROP TABLE Accounts
 -- DROP TABLE Contact_Us
--- SELECT * FROM Accounts
--- SELECT * FROM Contact_Us
-CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT, Email varchar(max),User_Name varchar(max), Password varchar(max))
-CREATE TABLE Contact_Us (ID int IDENTITY(1,1),  Email varchar(max),Question varchar(max))
+-- CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT, Email varchar(max),User_Name varchar(max), Password varchar(max))
+-- CREATE TABLE Contact_Us (ID int IDENTITY(1,1),  Email varchar(max),Question varchar(max))
 -- DROP TABLE Accounts
 -- DROP TABLE Contact_Us
 -- DROP TABLE TEST
+
+-- INSERT INTO [Accounts]
+    -- ( [Rank] , [Email] ,[User_Name] , [Password] )
+-- VALUES
+    -- (5,'go2ranuga@gmail.com','Programmer-RD-AI','dGVzdA==')
+SELECT * FROM Accounts
+SELECT * FROM Contact_Us
