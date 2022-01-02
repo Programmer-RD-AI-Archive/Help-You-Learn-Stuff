e2c241f8b91abb9cf591540a272908c0d1f35836
Programmer-RD-AI <aigithub@gmail.com>
2022-01-02 19:29:02 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import base64
+
+
+def encode(message: str) -> bytes:
+    """
+    Encode string for privacy and encryption.
+    """
+    msg_bytes = message.encode("latin-1")
+    string_bytes = base64.b64encode(msg_bytes)
+    string = string_bytes.decode("latin-1")
+    return string
+
+
+print(encode("test"))
