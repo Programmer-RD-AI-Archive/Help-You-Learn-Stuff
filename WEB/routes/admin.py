6efa61d5080c1bec66617bb442e3002995e45aa9
Programmer-RD-AI <aigithub@gmail.com>
2022-01-03 10:13:50 +0530

    Update admin.py
    aHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- WEB/routes/admin.py
+++ WEB/routes/admin.py
@@ -35,9 +35,10 @@ def admin_log_out():
 
 @app.route("/divinfo", methods=["POST"])
 def get_divinfo():
-    info = request.form["info"]
-    yourdiv = request.form["yourdiv"]
+    info = request.form.get("yourdiv")
+    # yourdiv = request.form["yourdiv"]
     print("*" * 50)
+    print(request.form)
     print(info)
     return ("", 200)
 
