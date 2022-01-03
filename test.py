3b94ef8a6bdcf3df1e3fdbfeac154f68acec0110
Programmer-RD-AI <aigithub@gmail.com>
2022-01-03 11:35:38 +0530

    Update test.py
    aHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
-import base64
+# import base64
 
 
-def encode(message: str) -> bytes:
-    """
-    Encode string for privacy and encryption.
-    """
-    msg_bytes = message.encode("latin-1")
-    string_bytes = base64.b64encode(msg_bytes)
-    string = string_bytes.decode("latin-1")
-    return string
+# def encode(message: str) -> bytes:
+#     """
+#     Encode string for privacy and encryption.
+#     """
+#     msg_bytes = message.encode("latin-1")
+#     string_bytes = base64.b64encode(msg_bytes)
+#     string = string_bytes.decode("latin-1")
+#     return string
 
 
-print(encode("test"))
+# print(encode("test"))
+
+{
+    '{"info":{"1":["trtret","gerger"]},"yourdiv":"<div class': '\\"mb-3\\"><label for=\\"1-Label\\" class=\\"form-label\\"><input type=\\"text\\" class=\\"form-control\\" id=\\"1-Input-Name\\"></label><input type=\\"text\\" class=\\"form-control\\" id=\\"1-Content\\"></div>"}'
+}
