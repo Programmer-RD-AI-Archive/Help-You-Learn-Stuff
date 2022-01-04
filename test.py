5c73499092dfdadf33122b8fdca217e3a977f914
Programmer-RD-AI <aigithub@gmail.com>
2022-01-04 11:53:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -37,14 +37,22 @@
 # yourdiv.replace(str(all_input[0]), info[str(idx)][0])
 from bs4 import BeautifulSoup, Tag
 
+# markup = '<a href="http://example.com/">I linked to <i>example.com</i></a>'
+# soup = BeautifulSoup(markup, "html.parser")
+# a_tag = soup.a
+# a_tag.string = "a"
+
+# print(a_tag)
+# new_tag = soup.new_tag("h1")
+# new_tag.string = "example.com"
+# a_tag.i.replace_with(new_tag)
+
+# print(a_tag)
+
 markup = '<a href="http://example.com/">I linked to <i>example.com</i></a>'
 soup = BeautifulSoup(markup, "html.parser")
 a_tag = soup.a
-a_tag.string = "a"
 
-print(a_tag)
-new_tag = soup.new_tag("h1")
+new_tag = soup.new_tag("b")
 new_tag.string = "example.com"
 a_tag.i.replace_with(new_tag)
-
-print(a_tag)
