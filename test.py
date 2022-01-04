3c9e2be55265317e07e44bc23a07e130ad0bdc4d
Programmer-RD-AI <aigithub@gmail.com>
2022-01-04 08:28:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,38 @@
 
 # print(encode("test"))
 
-{
-    '{"info":{"1":["trtret","gerger"]},"yourdiv":"<div class': '\\"mb-3\\"><label for=\\"1-Label\\" class=\\"form-label\\"><input type=\\"text\\" class=\\"form-control\\" id=\\"1-Input-Name\\"></label><input type=\\"text\\" class=\\"form-control\\" id=\\"1-Content\\"></div>"}'
-}
+# {
+#     '{"info":{"1":["trtret","gerger"]},"yourdiv":"<div class': '\\"mb-3\\"><label for=\\"1-Label\\" class=\\"form-label\\"><input type=\\"text\\" class=\\"form-control\\" id=\\"1-Input-Name\\"></label><input type=\\"text\\" class=\\"form-control\\" id=\\"1-Content\\"></div>"}'
+# }
+
+
+# soup = BeautifulSoup('<b class="boldest">Extremely bold</b>')
+# tag = soup.b
+
+# del tag["class"]
+# del tag["id"]
+
+# print(tag)
+
+
+# print(str(all_input[0]), info[str(idx)][0])
+# print(x)
+# soup = BeautifulSoup('<b class="boldest">Extremely bold</b>')
+# tag = soup.b
+# del tag["class"]
+# del tag["id"]
+# print(tag)
+# yourdiv.replace(str(all_input[0]), info[str(idx)][0])
+from bs4 import BeautifulSoup, Tag
+
+markup = '<a href="http://example.com/">I linked to <i>example.com</i></a>'
+soup = BeautifulSoup(markup, "html.parser")
+a_tag = soup.a
+a_tag.string = "a"
+
+print(a_tag)
+new_tag = soup.new_tag("h1")
+new_tag.string = "example.com"
+a_tag.i.replace_with(new_tag)
+
+print(a_tag)
