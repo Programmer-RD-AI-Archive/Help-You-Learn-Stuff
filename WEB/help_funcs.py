855dc646118fde3ac68f31fc2306bce22dd00781
Programmer-RD-AI <aigithub@gmail.com>
2022-01-06 20:49:47 +0530

    Update help_funcs.py

--- WEB/help_funcs.py
+++ WEB/help_funcs.py
@@ -1,4 +1,6 @@
 import base64
+import requests
+
 
 def encode(message: str) -> bytes:
     """
@@ -18,3 +20,17 @@ def decode(message: str) -> bytes:
     string_bytes = base64.b64decode(msg_bytes)
     string = string_bytes.decode("latin-1")
     return string
+
+
+def verify_email(email_address):
+    response = requests.get(
+        "https://isitarealemail.com/api/email/validate", params={"email": email_address}
+    )
+
+    status = response.json()["status"]
+    if status == "valid":
+        return True
+    elif status == "invalid":
+        return False
+    else:
+        return False
