5efb045f1bf07ba76d0af9dce2d2e15153256637
Programmer-RD-AI <aigithub@gmail.com>
2022-01-06 18:59:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,6 @@
 #     return string
 
 
-# print(encode("test"))
 
 # {
 #     '{"info":{"1":["trtret","gerger"]},"yourdiv":"<div class': '\\"mb-3\\"><label for=\\"1-Label\\" class=\\"form-label\\"><input type=\\"text\\" class=\\"form-control\\" id=\\"1-Input-Name\\"></label><input type=\\"text\\" class=\\"form-control\\" id=\\"1-Content\\"></div>"}'
@@ -24,16 +23,13 @@
 # del tag["class"]
 # del tag["id"]
 
-# print(tag)
 
 
-# print(str(all_input[0]), info[str(idx)][0])
-# print(x)
 # soup = BeautifulSoup('<b class="boldest">Extremely bold</b>')
 # tag = soup.b
 # del tag["class"]
 # del tag["id"]
-# print(tag)
+
 # yourdiv.replace(str(all_input[0]), info[str(idx)][0])
 from bs4 import BeautifulSoup, Tag
 
@@ -42,12 +38,9 @@ from bs4 import BeautifulSoup, Tag
 # a_tag = soup.a
 # a_tag.string = "a"
 
-# print(a_tag)
 # new_tag = soup.new_tag("h1")
 # new_tag.string = "example.com"
 # a_tag.i.replace_with(new_tag)
-
-# print(a_tag)
 
 markup = '<a href="http://example.com/">I linked to <i>example.com</i></a>'
 soup = BeautifulSoup(markup, "html.parser")
