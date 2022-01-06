84a0f5a64aa2369addcca897ab91c4dce5702a01
Programmer-RD-AI <aigithub@gmail.com>
2022-01-06 11:11:59 +0530

    Update azure_storage.py

--- API/db/azure_storage.py
+++ API/db/azure_storage.py
@@ -2,32 +2,28 @@ from API import *
 
 
 class Azure_Storage:
-    def __init__(self) -> None:
+    def __init__(self, container_name) -> None:
         self.connection_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
         self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_str)
-        self.container_name = str(uuid.uuid4())
+        self.container_name = container_name
         self.container_client = self.blob_service_client.create_container(self.container_name)
 
-    def create_file(self) -> None:
-        file = open("test.txt", "w")
-        file.write("Hello, World!")
-        file.close()
+    def create_file(self, blob_name, file_rb) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=f"test.txt"
+            container=self.container_name, blob=blob_name
         )
-        with open("./test.txt", "rb") as data:
-            blob_client.upload_blob(data)
+        blob_client.upload_blob(file_rb)
 
     def find_file(self) -> None:
         # iterate over all of the containers to find the files
         blobs_list = self.container_client.list_blobs()
+        blobs = []
         for blob in blobs_list:
-            print(blob.name + "\n")
+            blobs.append(blob)
+        return blobs
 
-    def download_file(self) -> None:
+    def download_file(self, file_name) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=f"test.txt"
+            container=self.container_name, blob=file_name
         )
-        download_file_path = "./downloaded_test.txt"
-        with open(download_file_path, "wb") as download_file:
-            download_file.write(blob_client.download_blob().readall())
+        return blob_client.download_blob().readall()
