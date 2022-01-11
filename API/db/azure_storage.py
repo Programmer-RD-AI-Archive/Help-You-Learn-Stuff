a433a127ed43ffdbd3639a92d6e986e733027b4c
Programmer-RD-AI <aigithub@gmail.com>
2022-01-11 11:44:14 +0530

    Update azure_storage.py

--- API/db/azure_storage.py
+++ API/db/azure_storage.py
@@ -3,27 +3,35 @@ from API import *
 
 class Azure_Storage:
     def __init__(self, container_name) -> None:
-        self.connection_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-        self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_str)
-        self.container_name = container_name
-        self.container_client = self.blob_service_client.create_container(self.container_name)
-
-    def create_file(self, blob_name, file_rb) -> None:
-        blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=blob_name
+        self.connection_str = "DefaultEndpointsProtocol=https;AccountName=helpyoulearnstuff;AccountKey=WMruG6IqnwGspaRB9vIL+SmhTwzM3iPE7cRtjHkikxpa7WJo5EvQ+rIqjFZIgoPqwmEvOCZ/4KSf42yVX8kkQQ==;EndpointSuffix=core.windows.net"
+        self.blob_service_client = BlobServiceClient.from_connection_string(
+            conn_str=self.connection_str
         )
-        blob_client.upload_blob(file_rb)
+        self.container_name = str(container_name)
+        try:
+            self.container_client = self.blob_service_client.create_container(self.container_name)
+        except:
+            pass
+
+    def create_file(self, file_rb, file_name_in_the_cloud) -> None:
+        blob_client = self.blob_service_client.get_blob_client(
+            container=self.container_name, blob=file_name_in_the_cloud
+        )
+        blob_client.upload_blob(file_rb, overwrite=True)
 
     def find_file(self) -> None:
         # iterate over all of the containers to find the files
         blobs_list = self.container_client.list_blobs()
-        blobs = []
+        files = []
         for blob in blobs_list:
-            blobs.append(blob)
-        return blobs
+            files.append(blob.name)
+        return files
 
-    def download_file(self, file_name) -> None:
+    def download_file(self, file_name_in_the_cloud) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=file_name
+            container=self.container_name, blob=file_name_in_the_cloud
         )
         return blob_client.download_blob().readall()
+
+    def delete_blob(self) -> None:
+        self.container_client.delete_container()
