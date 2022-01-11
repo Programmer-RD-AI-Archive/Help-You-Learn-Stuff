05d7af312b534095ab46e1ca57659057ad27ae10
Programmer-RD-AI <aigithub@gmail.com>
2022-01-11 11:44:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -47,39 +47,45 @@ from bs4 import BeautifulSoup, Tag
 # new_tag = soup.new_tag("b")
 # new_tag.string = "example.com"
 # a_tag.i.replace_with(new_tag)
+import os, uuid
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
-import os
 
 
 class Azure_Storage:
-    def __init__(self, container_name) -> None:
-        self.connection_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    def __init__(
+        self,
+    ) -> None:
+        self.connection_str = "DefaultEndpointsProtocol=https;AccountName=helpyoulearnstuff;AccountKey=WMruG6IqnwGspaRB9vIL+SmhTwzM3iPE7cRtjHkikxpa7WJo5EvQ+rIqjFZIgoPqwmEvOCZ/4KSf42yVX8kkQQ==;EndpointSuffix=core.windows.net"
         self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_str)
-        self.container_name = container_name
+        print(self.blob_service_client.list_blobs())
+        self.container_name = str("cources")
         self.container_client = self.blob_service_client.create_container(self.container_name)
 
-    def create_file(self, blob_name, file_rb) -> None:
+    def create_file(self, file_rb, file_name_in_the_cloud) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=blob_name
+            container=self.container_name, blob=file_name_in_the_cloud
         )
         blob_client.upload_blob(file_rb)
 
     def find_file(self) -> None:
         # iterate over all of the containers to find the files
         blobs_list = self.container_client.list_blobs()
-        blobs = []
+        files = []
         for blob in blobs_list:
-            blobs.append(blob)
-        return blobs
+            files.append(blob.name)
+        return files
 
-    def download_file(self, file_name) -> None:
+    def download_file(self, file_name_in_the_cloud) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=file_name
+            container=self.container_name, blob=file_name_in_the_cloud
         )
         return blob_client.download_blob().readall()
 
 
-a_storage = Azure_Storage()
-
-f = open("run.py", "rb")
-print(f.read())
+info = '{"1": ["trtret", "gerger"]}'
+info = bytes(info, encoding="utf-8")
+print(info)
+azure_storage = Azure_Storage()
+# azure_storage.create_file(file_name_in_the_cloud="/a/testa.json", file_rb=info)
+# azure_storage.find_file()
+# azure_storage.download_file()
