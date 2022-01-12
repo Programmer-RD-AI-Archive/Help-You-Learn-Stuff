9223c485472fa214449cf53fadb1703c597bbbb5
Programmer-RD-AI <aigithub@gmail.com>
2022-01-12 20:08:41 +0530

    Update help_funcs.py

--- API/help_funcs.py
+++ API/help_funcs.py
@@ -1,21 +1,43 @@
 from API import *
-import base64
+import smtplib
 
 
-def send_email(subject="TEST", message="From Python"):
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = "helpyoulearnstuff@gmail.com"  # Enter your address
-    receiver_email = "go2ranuga@gmail.com"  # Enter receiver address
-    password = "Ranuga D 2008"
-    message = f"""\
-    Subject: {subject}
-    
-    {message}"""
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+def send_email(subject="TEST", message="From Python", reviver="go2ranuga@gmail.com"):
+    """
+    Send Email using ttest
+    """
+
+    gmail_user = "helpyoulearnstuff@gmail.com"
+    gmail_password = "Ranuga D 2008"
+
+    sent_from = gmail_user
+    to = [reviver]
+    subject = subject
+    body = message
+
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (
+        sent_from,
+        ", ".join(to),
+        subject,
+        body,
+    )
+
+    try:
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        return True
+    except:
+        return False
 
 
 def encode(message: str) -> bytes:
