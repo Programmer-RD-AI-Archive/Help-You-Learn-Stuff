45e6b4f0d0750afb438629ce08aaa54de3ce6fa1
Programmer-RD-AI <aigithub@gmail.com>
2022-01-13 12:45:05 +0530

    Create questions.py

---
+++ API/routes/questions.py
@@ -0,0 +1,50 @@
+from API import *
+
+hp = Help_Funcs()
+questions = reqparse.RequestParser()
+questions.add_argument("html", type=str, help="html is required", required=True)
+questions.add_argument("name", type=str, help="name is required", required=True)
+
+
+class Questions(Resource):
+    def get(self) -> dict:
+        asql = Azure_SQL()
+        tables = asql.get_tables()
+        if "Questions" not in tables:
+            asql.create_new_table(
+                """
+                CREATE TABLE Questions
+                (
+                    [ID] int IDENTITY(1,1),
+                    [html] varchar(max),
+                    [name] varchar(max),
+                )
+                """
+            )
+        return {"message": asql.select_table(f"SELECT * FROM Questions")}
+
+    def post(self):
+        args = questions.parse_args()
+        print(args)
+        asql = Azure_SQL()
+        tables = asql.get_tables()
+        if "Questions" not in tables:
+            asql.create_new_table()
+        hp.table_exists_or_not(
+            "Questions",
+            """
+            CREATE TABLE Questions
+            (
+                [ID] int IDENTITY(1,1),
+                [html] varchar(max),
+                [name] varchar(max),
+            )
+            """,
+        )
+        asql.insert_to_table(
+            f"INSERT INTO Questions (html, name) VALUES ('{args['html']}','{args['name']}');"
+        )
+        return {"message": True}
+
+
+api.add_resource(Questions, "/api/questions")
