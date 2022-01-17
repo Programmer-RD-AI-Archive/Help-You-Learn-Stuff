4a9d63b0cc3e5d749e48c84e2643c1f9550cf769
Programmer-RD-AI <aigithub@gmail.com>
2022-01-17 20:23:17 +0530

    Update azure_storage.py

--- API/db/azure_storage.py
+++ API/db/azure_storage.py
@@ -3,19 +3,20 @@ from azure.storage.blob import BlobServiceClient, __version__
 
 class Azure_Storage:
     def __init__(self, container_name) -> None:
-        self.connection_str = "DefaultEndpointsProtocol=https;AccountName=helpyoulearnstuff;AccountKey=WMruG6IqnwGspaRB9vIL+SmhTwzM3iPE7cRtjHkikxpa7WJo5EvQ+rIqjFZIgoPqwmEvOCZ/4KSf42yVX8kkQQ==;EndpointSuffix=core.windows.net"
+        self.connection_str = """DefaultEndpointsProtocol=https;AccountName=helpyoulearnstuff;AccountKey=WMruG6IqnwGspaRB9vIL+SmhTwzM3iPE7cRtjHkikxpa7WJo5EvQ+rIqjFZIgoPqwmEvOCZ/4KSf42yVX8kkQQ==;EndpointSuffix=core.windows.net"""
         self.blob_service_client = BlobServiceClient.from_connection_string(
-            conn_str=self.connection_str)
+            conn_str=self.connection_str
+        )
         self.container_name = str(container_name)
         try:
-            self.container_client = self.blob_service_client.create_container(
-                self.container_name)
+            self.container_client = self.blob_service_client.create_container(self.container_name)
         except:
             pass
 
     def create_file(self, file_rb, file_name_in_the_cloud: str) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=file_name_in_the_cloud)
+            container=self.container_name, blob=file_name_in_the_cloud
+        )
         blob_client.upload_blob(file_rb, overwrite=True)
 
     def find_file(self) -> None:
@@ -28,7 +29,8 @@ class Azure_Storage:
 
     def download_file(self, file_name_in_the_cloud: str) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=file_name_in_the_cloud)
+            container=self.container_name, blob=file_name_in_the_cloud
+        )
         return blob_client.download_blob().readall()
 
     def delete_blob(self) -> None:
