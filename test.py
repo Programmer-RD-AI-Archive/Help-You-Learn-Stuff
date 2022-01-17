48ce552ced3aee95b8a4f49190595b792212d607
Programmer-RD-AI <aigithub@gmail.com>
2022-01-17 19:21:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,18 +4,19 @@ from azure.storage.blob import BlobServiceClient, __version__
 
 
 class Azure_Storage:
-    def __init__(self, ) -> None:
+    def __init__(
+        self,
+    ) -> None:
         self.connection_str = "DefaultEndpointsProtocol=https;AccountName=helpyoulearnstuff;AccountKey=WMruG6IqnwGspaRB9vIL+SmhTwzM3iPE7cRtjHkikxpa7WJo5EvQ+rIqjFZIgoPqwmEvOCZ/4KSf42yVX8kkQQ==;EndpointSuffix=core.windows.net"
-        self.blob_service_client = BlobServiceClient.from_connection_string(
-            self.connection_str)
-        print(self.blob_service_client.list_blobs())
+        self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_str)
+        # print(self.blob_service_client.list_blobs())
         self.container_name = str("cources")
-        self.container_client = self.blob_service_client.create_container(
-            self.container_name)
+        self.container_client = self.blob_service_client.create_container(self.container_name)
 
     def create_file(self, file_rb, file_name_in_the_cloud) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=file_name_in_the_cloud)
+            container=self.container_name, blob=file_name_in_the_cloud
+        )
         blob_client.upload_blob(file_rb)
 
     def find_file(self) -> None:
@@ -28,11 +29,11 @@ class Azure_Storage:
 
     def download_file(self, file_name_in_the_cloud) -> None:
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=file_name_in_the_cloud)
+            container=self.container_name, blob=file_name_in_the_cloud
+        )
         return blob_client.download_blob().readall()
 
 
 info = '{"1": ["trtret", "gerger"]}'
 info = bytes(info, encoding="utf-8")
-print(info)
 azure_storage = Azure_Storage()
