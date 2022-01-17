50699c273b884e5e4200abf3c6196cfa899a39d7
Programmer-RD-AI <aigithub@gmail.com>
2022-01-17 19:20:34 +0530

    Update help_funcs.py

--- API/help_funcs.py
+++ API/help_funcs.py
@@ -1,47 +1,38 @@
 import base64
 import smtplib
-
 from API import *
 
 
 class Help_Funcs:
-    def send_email(subject="TEST",
-                   message="From Python",
-                   reviver="go2ranuga@gmail.com"):
+    def send_email(subject: str, message: str, reviver: str) -> bool:
         """
         Send Email using ttest
         """
         gmail_user = config["Configs"]["Gmail"]
         gmail_password = config["Configs"]["Gmail Password"]
-        sent_from = gmail_user
-        to = [reviver]
-        body = message
         try:
             server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
             server.ehlo()
             server.login(gmail_user, gmail_password)
-            server.sendmail(sent_from, to, body)
+            server.sendmail(gmail_user, reviver, message)
             server.close()
             return True
         except:
             return False
 
-    def table_exists_or_not(self, table_name: str, query: str):
+    def table_exists_or_not(self, table_name: str, query: str) -> bool:
         try:
             asql = Azure_SQL()
             tables = asql.get_tables()
             if table_name not in tables:
-                asql.create_new_table(
-                    "CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT(max), Email varchar(max),User_Name varchar(max), Password varchar(max))"
-                )
+                asql.create_new_table(query)
+            return True
         except:
-            pass
+            return False
 
 
 class Encryption:
-    def __init__(self,
-                 message: str = "TEST",
-                 encoder: str = config["Configs"]["Encoder Type"]) -> None:
+    def __init__(self, message: str, encoder: str = config["Configs"]["Encoder Type"]) -> None:
         self.message = message
         self.encoder = encoder
 
