85e198b4fc313117c63de9ebd00fee97f1dffabe
Programmer-RD-AI <aigithub@gmail.com>
2022-01-18 21:30:46 +0530

    Update resources.py

--- API/routes/resources.py
+++ API/routes/resources.py
@@ -1,27 +1,18 @@
 from API import *
 
 resources_request_parser_delete = reqparse.RequestParser()
-resources_request_parser_delete.add_argument("id",
-                                             type=int,
-                                             help="id is required",
-                                             required=True)
+resources_request_parser_delete.add_argument("id", type=int, help="id is required", required=True)
 resources_request_parser = reqparse.RequestParser()
-resources_request_parser.add_argument("method_of_resource",
-                                      type=str,
-                                      help="method_of_resource is required",
-                                      required=True)
-resources_request_parser.add_argument("link_of_resource",
-                                      type=str,
-                                      help="link_of_resource is required",
-                                      required=True)
-resources_request_parser.add_argument("title",
-                                      type=str,
-                                      help="title is required",
-                                      required=True)
-resources_request_parser.add_argument("description",
-                                      type=str,
-                                      help="description is required",
-                                      required=True)
+resources_request_parser.add_argument(
+    "method_of_resource", type=str, help="method_of_resource is required", required=True
+)
+resources_request_parser.add_argument(
+    "link_of_resource", type=str, help="link_of_resource is required", required=True
+)
+resources_request_parser.add_argument("title", type=str, help="title is required", required=True)
+resources_request_parser.add_argument(
+    "description", type=str, help="description is required", required=True
+)
 
 
 class Resources(Resource):
@@ -52,9 +43,7 @@ class Resources(Resource):
         args = resources_request_parser_delete.parse_args()
         asql = Azure_SQL()
         return {
-            "message":
-            asql.insert_to_table(
-                f"DELETE FROM Resources WHERE ID={args['id']}")
+            "message": asql.insert_to_table(f"DELETE FROM Resources WHERE ID={args['id']}")
         }  # TODO
 
     def put(self):
