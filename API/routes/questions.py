f7157f667104a941895f60d2ad3d559dc4c16429
Programmer-RD-AI <aigithub@gmail.com>
2022-01-18 19:36:35 +0530

    Update questions.py

--- API/routes/questions.py
+++ API/routes/questions.py
@@ -2,32 +2,47 @@ from API import *
 
 hp = Help_Funcs()
 questions = reqparse.RequestParser()
-questions.add_argument("html",
-                       type=str,
-                       help="html is required",
-                       required=True)
-questions.add_argument("name",
-                       type=str,
-                       help="name is required",
-                       required=True)
+questions.add_argument("html", type=str, help="html is required", required=True)
+questions.add_argument("name", type=str, help="name is required", required=True)
 
 
 class Questions(Resource):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
     def get(self) -> dict:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
         asql = Azure_SQL()
         tables = asql.get_tables()
         if "Questions" not in tables:
-            asql.create_new_table("""
+            asql.create_new_table(
+                """
                 CREATE TABLE Questions
                 (
                     [ID] int IDENTITY(1,1),
                     [html] varchar(max),
                     [name] varchar(max),
                 )
-                """)
+                """
+            )
         return {"message": asql.select_table("SELECT * FROM Questions")}
 
     def post(self):
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
         args = questions.parse_args()
         asql = Azure_SQL()
         tables = asql.get_tables()
