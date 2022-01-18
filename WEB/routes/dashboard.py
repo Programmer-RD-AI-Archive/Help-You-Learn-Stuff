26e4922b48292180a11fe033b5977ee0b3415652
Programmer-RD-AI <aigithub@gmail.com>
2022-01-18 18:37:11 +0530

    Update dashboard.py

--- WEB/routes/dashboard.py
+++ WEB/routes/dashboard.py
@@ -5,17 +5,19 @@ from WEB.help_funcs import *
 @app.route("/Usr/<_id>", methods=["GET", "POST"])
 @app.route("/Usr/<_id>/", methods=["GET", "POST"])
 def usr_home(_id):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     if str(_id) == str(session["id"]):
         password = "01x2253x6871"
-        config = requests.get("http://127.0.0.1:5000/api/get_config",
-                              {"password": password})
+        config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
         config = config.json()
         courses = requests.get(
             "http://127.0.0.1:5000/api/azure/sql",
-            {
-                "Query": "SELECT * FROM Courses",
-                "Type": "Select"
-            },
+            {"Query": "SELECT * FROM Courses", "Type": "Select"},
         ).json()["message"]
         new_cources = []
         iter_cources = []
@@ -40,26 +42,24 @@ def usr_home(_id):
 @app.route("/Usr/<_id>/Cources/<course_id>/", methods=["GET", "POST"])
 @app.route("/Usr/<_id>/Cources/<course_id>", methods=["GET", "POST"])
 def usr_home_cources(_id, course_id):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     if str(_id) == str(session["id"]):
         password = "01x2253x6871"
-        config = requests.get("http://127.0.0.1:5000/api/get_config",
-                              {"password": password})
+        config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
         config = config.json()
         courses = requests.get(
             "http://127.0.0.1:5000/api/azure/sql",
-            {
-                "Query": f"SELECT * FROM Courses WHERE ID={course_id}",
-                "Type": "Select"
-            },
+            {"Query": f"SELECT * FROM Courses WHERE ID={course_id}", "Type": "Select"},
         ).json()["message"]
         info = courses[0][2]
         info = requests.get(
             "http://127.0.0.1:5000/api/azure/storage",
-            {
-                "Type": "Download File",
-                "Container Name": "cource",
-                "file_name": info
-            },
+            {"Type": "Download File", "Container Name": "cource", "file_name": info},
         ).json()["message"]
         info = dict(eval(info))
         session[f"Cource {course_id}"] = info
@@ -68,11 +68,15 @@ def usr_home_cources(_id, course_id):
         return redirect(f"/Usr/{_id}/Cources/{course_id}/Lesson/{next_lesson}")
 
 
-@app.route("/Usr/<_id>/Cources/<course_id>/Lesson/<lesson_id>/",
-           methods=["GET", "POST"])
-@app.route("/Usr/<_id>/Cources/<course_id>/Lesson/<lesson_id>",
-           methods=["GET", "POST"])
+@app.route("/Usr/<_id>/Cources/<course_id>/Lesson/<lesson_id>/", methods=["GET", "POST"])
+@app.route("/Usr/<_id>/Cources/<course_id>/Lesson/<lesson_id>", methods=["GET", "POST"])
 def usr_home_cource_lesson(_id, course_id, lesson_id):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     if str(_id) == str(session["id"]):
         if int(lesson_id) <= len(session[f"Cource {course_id}"]):
             specific_lesson_info = session[f"Cource {course_id}"][lesson_id]
@@ -80,14 +84,12 @@ def usr_home_cource_lesson(_id, course_id, lesson_id):
                 info_of_page = requests.get(
                     "http://127.0.0.1:5000/api/azure/sql",
                     {
-                        "Query":
-                        f"SELECT * FROM Resources WHERE ID={int(specific_lesson_info[0][1])}",
+                        "Query": f"SELECT * FROM Resources WHERE ID={int(specific_lesson_info[0][1])}",
                         "Type": "Select",
                     },
                 ).json()["message"]
                 if info_of_page[0][1] == 1:
-                    info_of_page[0][2] = ("https://www.youtube.com/embed/" +
-                                          info_of_page[0][2])
+                    info_of_page[0][2] = "https://www.youtube.com/embed/" + info_of_page[0][2]
                 return render_template(
                     "dashboard/lesson.html",
                     url=info_of_page[0][2],
@@ -99,8 +101,7 @@ def usr_home_cource_lesson(_id, course_id, lesson_id):
             info_of_page = requests.get(
                 "http://127.0.0.1:5000/api/azure/sql",
                 {
-                    "Query":
-                    f"SELECT * FROM Questions WHERE ID={int(specific_lesson_info[0][1])}",
+                    "Query": f"SELECT * FROM Questions WHERE ID={int(specific_lesson_info[0][1])}",
                     "Type": "Select",
                 },
             ).json()["message"]
@@ -115,6 +116,12 @@ def usr_home_cource_lesson(_id, course_id, lesson_id):
 @app.route("/Usr/<_id>/Log/Out", methods=["GET", "POST"])
 @app.route("/Usr/<_id>/Log/Out/", methods=["GET", "POST"])
 def usr_logout(_id):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     if str(_id) == str(session["id"]):
         session.pop("id", None)
         session.pop("User Name", None)
