613a9cef8a1a1ec5c2be99eb80089fc062e8f9e1
Programmer-RD-AI <aigithub@gmail.com>
2022-01-18 19:36:26 +0530

    Update contact_us.py

--- API/routes/contact_us.py
+++ API/routes/contact_us.py
@@ -2,24 +2,32 @@ from API import *
 
 hp = Help_Funcs()
 contact_us_request_parser = reqparse.RequestParser()
-contact_us_request_parser.add_argument("email",
-                                       type=str,
-                                       help="email is required",
-                                       required=True)
-contact_us_request_parser.add_argument("question",
-                                       type=str,
-                                       help="question is required",
-                                       required=True)
+contact_us_request_parser.add_argument("email", type=str, help="email is required", required=True)
+contact_us_request_parser.add_argument(
+    "question", type=str, help="question is required", required=True
+)
 
 
 class Contact_Us(Resource):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
     def post(self) -> dict:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
         try:
             args = contact_us_request_parser.parse_args()
             email = args["email"]
             question = args["question"]
-            send_email(subject=question,
-                       message=f"Email - {email} \n Question {question}")
+            send_email(subject=question, message=f"Email - {email} \n Question {question}")
             asql = Azure_SQL()
             hp.table_exists_or_not(
                 "Contact_Us",
