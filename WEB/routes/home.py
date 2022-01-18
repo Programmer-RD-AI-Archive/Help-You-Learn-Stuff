17de5c667abcafe45bf6b3f3694d598ebd72e009
Programmer-RD-AI <aigithub@gmail.com>
2022-01-18 18:37:15 +0530

    Update home.py

--- WEB/routes/home.py
+++ WEB/routes/home.py
@@ -4,25 +4,37 @@ from WEB.help_funcs import *
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     if request.method == "POST":
         email = request.form["email"]
         question = request.form["Question"]
-        already_accounts = requests.get("http://127.0.0.1:5000/api/Accounts", )
+        already_accounts = requests.get(
+            "http://127.0.0.1:5000/api/Accounts",
+        )
         already_accounts = already_accounts.json()
         ok = None
         for already_account in already_accounts["message"]:
-            if (already_account[2] == email
-                    and already_account[4] == encode(question)
-                    and already_account[1] == 5):
+            if (
+                already_account[2] == email
+                and already_account[4] == encode(question)
+                and already_account[1] == 5
+            ):
                 ok = True
                 password = already_account[4]
                 email = already_account[2]
                 user_name = already_account[3]
                 rank = already_account[1]
                 _id = already_account[0]
-            elif (already_account[3] == email
-                  and already_account[4] == encode(question)
-                  and already_account[1] == 5):
+            elif (
+                already_account[3] == email
+                and already_account[4] == encode(question)
+                and already_account[1] == 5
+            ):
                 ok = True
                 password = already_account[4]
                 email = already_account[2]
@@ -39,10 +51,7 @@ def home():
             return redirect("/Admin/")
         config = requests.post(
             "http://127.0.0.1:5000/api/Contact_Us",
-            {
-                "email": email,
-                "question": question
-            },
+            {"email": email, "question": question},
         )
         config = config.json()
         if config["message"] is True:
@@ -61,16 +70,14 @@ def home():
         flash("There is some error so please try again.", "danger")
         return redirect("/")
     password = "01x2253x6871"
-    config = requests.get("http://127.0.0.1:5000/api/get_config",
-                          {"password": password})
+    config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
     config = config.json()
     return render_template(
         "home/home.html",
         gif_1=config["config"]["Home"]["Help you Learn Stuff"]["Gif"],
         gif_2=config["config"]["Home"]["About Us"]["Gif"],
         gif_3=config["config"]["Home"]["Contact Us"]["Gif"],
-        description_1=config["config"]["Home"]["Help you Learn Stuff"]
-        ["Description"],
+        description_1=config["config"]["Home"]["Help you Learn Stuff"]["Description"],
         description_2=config["config"]["Home"]["About Us"]["Description"],
         description_3=config["config"]["Home"]["Contact Us"]["Description"],
         session=session,
@@ -80,9 +87,14 @@ def home():
 @app.route("/Sign/Up", methods=["GET", "POST"])
 @app.route("/Sign/Up/", methods=["GET", "POST"])
 def sign_up():
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     password = "01x2253x6871"
-    config = requests.get("http://127.0.0.1:5000/api/get_config",
-                          {"password": password})
+    config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
     config = config.json()
     if request.method == "POST":
         email = request.form["Email"]
@@ -91,24 +103,20 @@ def sign_up():
         if verify_email(email) is False:
             flash("Invalid Email", "danger")
             return redirect("/Sign/Up")
-        already_accounts = requests.get("http://127.0.0.1:5000/api/Accounts", )
+        already_accounts = requests.get(
+            "http://127.0.0.1:5000/api/Accounts",
+        )
         already_accounts = already_accounts.json()
         for already_account in already_accounts["message"]:
-            if already_account[1] == email and already_account[3] == encode(
-                    password):
+            if already_account[1] == email and already_account[3] == encode(password):
                 flash("Email is already exist.", "danger")
                 return redirect("/Sign/Up")
-            if already_account[2] == user_name and already_account[
-                    3] == encode(password):
+            if already_account[2] == user_name and already_account[3] == encode(password):
                 flash("User Name is already exist.", "danger")
                 return redirect("/Sign/Up")
         account_add = requests.post(
             "http://127.0.0.1:5000/api/Accounts",
-            {
-                "email": email,
-                "password": encode(password),
-                "user_name": user_name
-            },
+            {"email": email, "password": encode(password), "user_name": user_name},
         )
         account_add = account_add.json()
         session["Email or User Name"] = email
@@ -121,26 +129,33 @@ def sign_up():
 @app.route("/Sign/In", methods=["GET", "POST"])
 @app.route("/Sign/In/", methods=["GET", "POST"])
 def sign_in():
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     password = "01x2253x6871"
-    config = requests.get("http://127.0.0.1:5000/api/get_config",
-                          {"password": password})
+    config = requests.get("http://127.0.0.1:5000/api/get_config", {"password": password})
     config = config.json()
     if request.method == "POST":
         user_name_or_email = request.form["Email or User Name"]
         password = request.form["Password"]
-        already_accounts = requests.get("http://127.0.0.1:5000/api/Accounts", )
+        already_accounts = requests.get(
+            "http://127.0.0.1:5000/api/Accounts",
+        )
         already_accounts = already_accounts.json()
         ok = None
         for already_account in already_accounts["message"]:
-            if already_account[2] == user_name_or_email and already_account[
-                    4] == encode(password):
+            if already_account[2] == user_name_or_email and already_account[4] == encode(password):
                 email = already_account[2]
                 user_name = already_account[3]
                 _id = already_account[0]
                 rank = already_account[1]
                 ok = True
-            elif already_account[3] == user_name_or_email and already_account[
-                    4] == encode(password):
+            elif already_account[3] == user_name_or_email and already_account[4] == encode(
+                password
+            ):
                 email = already_account[2]
                 user_name = already_account[3]
                 _id = already_account[0]
