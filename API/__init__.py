30be9cba29e133f23e6fba71bf9afafd6ef06121
Programmer-RD-AI <aigithub@gmail.com>
2022-01-19 12:29:36 +0530

    Update __init__.py

--- API/__init__.py
+++ API/__init__.py
@@ -3,10 +3,7 @@ import warnings
 
 from flask import *
 from flask_restful import *
-
-from API.db import *
-from API.help_funcs import *
-from API.routes import *
+from flask_restful import reqparse
 
 config = json.load(open("./API/config.json"))
 password = config["Configs"]["API Key"]
@@ -15,3 +12,6 @@ app.debug = True  # debug
 app.secret_key = config["Configs"]["Secret Key"]  # secret key
 app.config["SECURITY_PASSWORD_SALT"] = config["Configs"]["Secret Key"]
 api = Api(app)
+from API.db import *
+from API.help_funcs import *
+from API.routes import *
